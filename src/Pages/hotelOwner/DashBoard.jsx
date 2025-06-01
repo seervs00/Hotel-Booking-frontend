b979a65db9ca5b26a118bd9203557e1f92bff1ca
mahendra <mahendras00b@gmail.com>
2025-06-01 23:00:12 +0530

    complete frontend using dummy data

--- src/Pages/hotelOwner/DashBoard.jsx
+++ src/Pages/hotelOwner/DashBoard.jsx
@@ -31,7 +31,37 @@ const DashBoard = () => {
                 </div>
             </div>
          </div>
+         {/* Recent Bookings */}
+          <h2 className='text-xl text-blue-950/70 font-medium mb-5'>
+            Recent Bookings</h2>
+            <div className='w-full max-w-3xl text-left border border-gray-300 rounded-lg max-h-80 overflow-y-scroll'>
+                <table className='w-full'>
+                    <thead className='bg-gray-50'> 
+                        <tr>
+                        <th className='py-3  px-4 text-gray-800 font medium'>User Name</th>
+                        <th className='py-3 px-4 text-gray-800 font medium mx-sm:hidden'>Room Name</th>
+                        <th className='py-3 px-4 text-gray-800 font medium text-center'>Total Amount</th>
+                        <th className='py-3 px-4 text-gray-800 font medium text-center'>Payment Status</th>
+                        </tr>
+                    </thead>
 
+                    <tbody className='text-sm'>
+                        {dashboardData.bookings.map((item,index) => (
+                            <tr key={index}>
+                                <td className='py-3 px-4 text-gray-700 border-t border-gray-300'>{item.user.username}</td>
+                                <td className='py-3 px-4 text-gray-700 border-t border-gray-300 mx-sm:hidden'>{item.room.roomType}</td>
+                                <td className='py-3 px-4 text-gray-700 border-t border-gray-300 text-center'> ${item.totalPrice}</td>
+                                <td className='py-3 px-4 border-t border-gray-300 flex'>
+                                    <button className={`py-1 px-3 text-xs rounded-full mx-auto 
+                                        ${item.isPaid ? 'bg-green-200 text-green-600' : 'bg-amber-200 text-yellow-600'}`}>
+                                        {item.isPaid ? 'Cpmpleted':"pending"}
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
     </div>
 
   )
